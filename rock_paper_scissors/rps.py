d677db43f7abe5742130435633c01e99bacec775
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-12-11 14:21:06 -0600

    Completed rps

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,23 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  list_of_plays = []
+  possible_moves = ['rock', 'paper', 'scissors']
+
+  def rps_plays(moves, n):
+    if n > 0:
+      # will loop through and add the possible move to the moves that are done each time.
+      for i in range(len(possible_moves)):
+        rps_plays(moves + [possible_moves[i]], n-1)
+    elif n == 0:
+      # This will append whatever moves they want set to n = 0
+      return list_of_plays.append(moves)
+
+  if n > 0:
+    rps_plays([], n)
+    return list_of_plays
+  elif n == 0:
+    return [[]]
 
 
 if __name__ == "__main__":
