6e0d6bba2d8d5b95ef087a92be9d004b6666e4f9
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-12-11 15:07:53 -0600

    Added notes to stock prices to explain code

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,15 +3,17 @@
 import argparse
 
 def find_max_profit(prices):
-    max_prof = -max(prices)
-    arr = prices.copy()
-    for x in range(len(prices)):
-        arr.pop(0)
-        for i in arr:
-            if i - prices[x] > max_prof:
-                max_prof = i - prices[x]
-    print(max_prof)
-    return max_prof
+  max_prof = -max(prices)
+  #Can't change the prices array because you need if for the if statement later, so use copy()
+  arr = prices.copy()
+  for x in range(len(prices)):
+    #Take the first value out of the array
+    arr.pop(0)
+    for i in arr:
+      # It goes through the array, and sets the max profit each time. If the max profit is bigger for a different point, then the max profit will be set again, otherwise it goes through the loop again without setting it.
+      if i - prices[x] > max_prof:
+        max_prof = i - prices[x]
+  return max_prof
 
 
 if __name__ == '__main__':
